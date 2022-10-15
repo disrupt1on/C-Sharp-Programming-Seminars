16f5d1f9671c7a2cfa51601e457c0873c0871682
MariaTrubkina <trubkinamaria48@gmail.com>
2022-10-15 21:40:05 +0300

    added Home Task 6

---
+++ Seminar-1/Home-Task-6/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Введите число: ");
+int Number = Convert.ToInt32(Console.ReadLine());
+
+if (Number % 2 == 0)
+{
+    Console.Write($"Число {Number} является чётным.");
+}
+else
+{
+    Console.Write($"Число {Number} являестя нечётным.");
+}
