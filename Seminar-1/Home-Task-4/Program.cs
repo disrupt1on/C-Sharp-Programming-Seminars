486bda78c965c845d2c364706fe49cb9557491ae
MariaTrubkina <trubkinamaria48@gmail.com>
2022-10-15 21:38:46 +0300

    added Home Task 4

---
+++ Seminar-1/Home-Task-4/Program.cs
@@ -0,0 +1,27 @@
+﻿Console.Write("Введите число 1: ");
+int Number_1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число 2: ");
+int Number_2 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число 3: ");
+int Number_3 = Convert.ToInt32(Console.ReadLine());
+int Number_Max = 0;
+
+
+if (Number_1 > Number_2)
+{
+    Number_Max = Number_1;
+}
+else
+{
+    Number_Max = Number_2;
+}
+
+if (Number_3 > Number_Max)
+{
+    Number_Max = Number_3;
+    Console.Write($"Максимальное число = {Number_Max}");
+}
+else
+{
+    Console.WriteLine($"Максимальное число = {Number_Max}");
+}
