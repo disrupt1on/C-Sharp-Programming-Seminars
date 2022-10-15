82dc65377e9be9ef6e552c24fa7e88adc88418d4
MariaTrubkina <trubkinamaria48@gmail.com>
2022-10-15 21:40:51 +0300

    added Home Task 8

---
+++ Seminar-1/Home-Task-8/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите число N: ");
+int Number_N = Convert.ToInt32(Console.ReadLine());
+
+int i = 1;
+while (i <= Number_N)
+{
+    if (i % 2 == 0)
+    {
+        Console.WriteLine(i);
+    }
+    i = i + 1;
+}
