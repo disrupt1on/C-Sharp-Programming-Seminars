4a9c3a4c4328318dc8c952a1face12a28018948f
MariaTrubkina <trubkinamaria48@gmail.com>
2022-10-15 21:37:05 +0300

    added Home Task 2

---
+++ Seminar-1/Home-Task-2/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Write("Введите число 1: ");
+int Number_1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число 2: ");
+int Number_2 = Convert.ToInt32(Console.ReadLine());
+
+if (Number_1 > Number_2)
+{
+    Console.Write($"Максимальное число = {Number_1}, а минимальное число = {Number_2}");
+}
+else
+{
+    Console.Write($"Максимальное число = {Number_2}, а минимальное число = {Number_1}");
+}
