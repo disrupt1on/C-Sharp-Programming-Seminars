c7a564fa453cc5588f344a9e9e71a0b91cc86dc8
MariaTrubkina <trubkinamaria48@gmail.com>
2022-10-20 11:23:13 +0300

    completed home task 13

---
+++ Seminar-2/Home-Task-13/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int a3 = number % 10; // третья цифра в числах < 1000
+int a3_1 = (number / 100) % 10; // третья цифра в числах > 1000
+
+if (number < 100)
+{
+    Console.WriteLine("Третьей цифры нет");
+}
+else
+{
+  if (number < 1000)
+  {
+    Console.WriteLine(a3);
+  }
+  else
+  {
+    Console.WriteLine(a3_1);
+  }
+}
