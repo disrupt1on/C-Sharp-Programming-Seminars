0e6872e9dc76abc42ba9444383b6ffaae16d5cd2
MariaTrubkina <trubkinamaria48@gmail.com>
2022-10-20 11:41:56 +0300

    completed home task 15

---
+++ Seminar-2/Home-Task-15/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.WriteLine("Введите цифру дня недели: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number > 0 && number < 8)
+{
+    if (number == 6 || number == 7)
+    {
+        Console.WriteLine("Этот день является выходным");
+    }
+    else
+    {
+    Console.WriteLine("Этот день не является выходным");
+    }
+}
+else
+{
+    Console.WriteLine("Такого дня недели нет!");
+}
