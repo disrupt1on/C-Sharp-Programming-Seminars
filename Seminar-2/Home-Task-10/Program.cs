2316a20494e2727ffd3676698ff50feae52c940e
MariaTrubkina <trubkinamaria48@gmail.com>
2022-10-20 11:45:10 +0300

    made change

--- Seminar-2/Home-Task-10/Program.cs
+++ Seminar-2/Home-Task-10/Program.cs
@@ -1,7 +1,6 @@
 ﻿Console.WriteLine("Введите трёхзначное число: ");
 int number = Convert.ToInt32(Console.ReadLine());
 
-int a1 = number / 100; // первая цифра в числе
 int a2 = (number / 10) % 10; // вторая цифра в числе
 
 Console.WriteLine($"Вторая цифра в числе = {a2}");
