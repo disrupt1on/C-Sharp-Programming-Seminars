9f14bd8da5abd3fde1550e7c411cb1b10d0b342c
MariaTrubkina <trubkinamaria48@gmail.com>
2022-10-26 13:50:59 +0300

    completed hometask 23

---
+++ Seminar-3/Home-Task-23/Program.cs
@@ -0,0 +1,14 @@
+﻿// Напишите программу, которая принимает на вход число (N) 
+//и выдаёт таблицу кубов чисел от 1 до N.
+
+void PrintSquaresToConsole(int n)
+{
+    for (int i = 1; i <= n; i++)
+    {
+        Console.WriteLine(i * i * i);
+    }
+}
+
+Console.Write("Введите число N: ");
+int n = Convert.ToInt32(Console.ReadLine());
+PrintSquaresToConsole(n);
