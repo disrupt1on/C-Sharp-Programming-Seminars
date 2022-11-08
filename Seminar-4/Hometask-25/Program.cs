2836710bcfda7312f20cee8a9fcee0098259d670
MariaTrubkina <trubkinamaria48@gmail.com>
2022-11-08 17:11:53 +0300

    completed hometask 25

---
+++ Seminar-4/Hometask-25/Program.cs
@@ -0,0 +1,22 @@
+﻿//Напишите программу, которая принимает на вход два числа (A и B) и возводит число A в натуральную 
+//степень B. Для решения задания использование цикла for является обязательным условием. 
+//Не использовать встроенный метод возведения в степень
+
+int GetNumberGrade(int numberA, int numberB)
+{
+    int grade = numberA;
+    for (int i = 1; i < numberB; i++)
+    {
+        grade = grade * numberA;
+    }
+    return grade;  
+}
+
+Console.Write("Введите число A: ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите число B: ");
+int numberB = Convert.ToInt32(Console.ReadLine());
+
+int numberGrade = GetNumberGrade(numberA, numberB);
+Console.WriteLine(numberGrade);
