ae9009d4c3e57d01d5865b4ac111c9f7401c1c59
MariaTrubkina <trubkinamaria48@gmail.com>
2022-11-11 11:12:25 +0300

    hometask 27 completed

---
+++ Seminar-4/Hometask-27/Program.cs
@@ -0,0 +1,21 @@
+﻿//Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе. 
+//Для решения задания использование цикла for является обязательным условием
+
+int GetNumeralsSum(int number)
+{
+    int numeral = number % 10;
+    int sum = 0;
+
+    for( ; number > 0; )
+    {
+        sum = sum + numeral;
+        number = number / 10;
+    }
+    return sum;
+ }
+
+Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int sum = GetNumeralsSum(number);
+Console.WriteLine(sum);
