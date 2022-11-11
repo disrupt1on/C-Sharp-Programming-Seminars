f577877716bc9cd4e28667f40cb7534927edccd0
MariaTrubkina <trubkinamaria48@gmail.com>
2022-11-11 11:11:55 +0300

    hometask 29 completed

---
+++ Seminar-4/Hometask-29/Program.cs
@@ -0,0 +1,18 @@
+﻿//Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран. 
+//Заполнение массива производит пользователь.
+
+Console.Write("Введите желаемое количество строк: ");
+int rowsCount = Convert.ToInt32(Console.ReadLine());
+
+string[] array = new string[rowsCount];
+for (int i = 0; i < rowsCount; i++)
+{
+    Console.Write($"Введите элемент для строки №{i}: ", i + 1);
+    array[i] = Console.ReadLine();
+}
+
+for (int index = 0; index < array.Length - 1; index++)
+{
+    Console.Write(array[index] + ", ");
+}
+Console.Write(array[array.Length - 1]);
