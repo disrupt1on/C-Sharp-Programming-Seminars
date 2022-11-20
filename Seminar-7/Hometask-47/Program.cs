852c56dd2e4b6c8b587fbd70405341c7c97cd71a
Maria Trubkina <trubkinamaria48@gmail.com>
2022-11-20 23:58:56 +0300

    completet hometask 47

---
+++ Seminar-7/Hometask-47/Program.cs
@@ -0,0 +1,27 @@
+﻿/*Запросите от пользователя размерности двумерного массива. 
+Напишите метод для заполнения массива случайными числами. 
+Напишите метод для вывода массива на экран (постарайтесь вывести массив красиво). 
+Округлите значения, генерируемые Random до двух знаков после запятой.*/
+
+void fillAndPrintOutArray(double [ , ] array, int rows, int columns)
+{
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < columns; j++ )
+        {
+            array[i,  j] = Math.Round(new Random().NextDouble() * 100, 2);
+            Console.Write(array[i,  j] + " ");
+            Console.Write(" ");
+        }
+        Console.WriteLine();
+    }
+}
+
+Console.Write("Введите желаемое количество строк: ");
+int rows = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите желаемое количество колонок: ");
+int columns = Convert.ToInt32(Console.ReadLine());
+
+double [ , ] array = new double [rows, columns];
+fillAndPrintOutArray(array, rows, columns);
