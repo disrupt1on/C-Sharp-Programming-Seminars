5e1cbc050a108bd6240da93c1e16bd8def6d0602
Maria Trubkina <trubkinamaria48@gmail.com>
2022-11-23 01:23:49 +0300

    added seminar 7 folder

---
+++ Seminar-7/Task-48/Program.cs
@@ -0,0 +1,23 @@
+﻿/* Задайте двумерный массив размера m на n, 
+каждый элемент в массиве находится по формуле Amn = m + n*/
+
+Console.Write("Введите желаемое количество строк: ");
+int rows = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите желаемое количество колонок: ");
+int columns = Convert.ToInt32(Console.ReadLine());
+
+int [ , ] array = new int [rows, columns];
+
+for (int i = 0; i < rows; i++)
+{
+    for (int j = 0; j < columns; j++ )
+    {
+            array[i,  j] = i + j;
+            Console.Write(array[i,  j] + " ");
+    }
+    Console.WriteLine();
+}
+
+//array.Length - количество элементов внутри массива
+//array.GetLength - возвращает целое число элементов в определённом измерении
---
+++ Seminar-7/Task-49/Program.cs
@@ -0,0 +1,42 @@
+﻿/* Задайте двумерный массив. Найдите элементы, у которых оба индекса чётные, 
+и замените эти элементы на их квадраты*/
+
+void newArray(int[ , ] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++ )
+        {
+            if(i % 2 == 0 && j % 2 == 0)
+            {
+                array[i, j] = array[i, j] * array[i, j];
+                Console.Write(array[i,  j] + " ");
+            }
+            else
+            {
+                Console.Write(array[i,  j] + " ");
+            }
+        }
+        Console.WriteLine();
+    }
+}
+
+Console.Write("Введите желаемое количество строк: ");
+int rows = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите желаемое количество колонок: ");
+int columns = Convert.ToInt32(Console.ReadLine());
+
+int [ , ] array = new int [rows, columns];
+for (int i = 0; i < rows; i++)
+{
+    for (int j = 0; j < columns; j++ )
+    {
+            array[i,  j] = new Random().Next(1, 10);
+            Console.Write(array[i,  j] + " ");
+    }
+    Console.WriteLine();
+}
+Console.WriteLine(" ");
+
+newArray(array);
