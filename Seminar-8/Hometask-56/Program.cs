6d6907b5b1e3ee8fec67a2fbfbfba06b47273eb5
MariaTrubkina <trubkinamaria48@gmail.com>
2022-11-27 19:26:22 +0300

    completed hometask 56

--- Seminar-8/Hometask-56/Program.cs
+++ Seminar-8/Hometask-56/Program.cs
@@ -1,39 +1,49 @@
 ﻿/* Задайте прямоугольный двумерный массив. Напишите программу, 
 которая будет находить строку с наименьшей суммой элементов.*/
 
+void GetRowNumberAndSum(int[,] array)
+{
+    int row = 0;
+    int minSum = 0;
+    for (int i = 0; i < array.GetLength(1); i++)
+    {
+        minSum = minSum + array[0, i];
+    }
+
+    for (int i = 1; i < array.GetLength(0); i++)
+    {
+        int sum = 0;
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            sum = sum + array[i, j];
+        }
+
+        if (minSum > sum)
+        {
+            minSum = sum;
+            row = i;
+        }
+    }
+    Console.WriteLine($"Строка {row + 1}, содержит наименьшую сумму элементов = {minSum}");
+}
+
 Console.Write("Введите желаемое количество строк: ");
 int rows = Convert.ToInt32(Console.ReadLine());
 
 Console.Write("Введите желаемое количество колонок: ");
 int columns = Convert.ToInt32(Console.ReadLine());
 
-int[ , ] array = new int[rows, columns];
-for(int i = 0; i < array.GetLength(0); i++)
+int[,] array = new int[rows, columns];
+for (int i = 0; i < array.GetLength(0); i++)
 {
-    for(int j = 0; j < array.GetLength(1); j++)
+    for (int j = 0; j < array.GetLength(1); j++)
     {
-        array[i,  j] = new Random().Next(100);
-        Console.Write(array[i,  j] + " ");
+        array[i, j] = new Random().Next(100);
+        Console.Write(array[i, j] + " ");
     }
     Console.WriteLine();
 }
-
 Console.WriteLine();
 
-int minSum = 0;
-int rowWithMinSum = 0;
-for (int i = 0; i <= array.GetLength(1) - 1; i++)
-{
-    int sum = 0;
-    for (int j = 0; j <= array.GetLength(0) - 1; j++ )
-    {
-        sum += array[j, i];
-        
-        if(minSum > sum)
-        {
-            minSum = sum;
-            rowWithMinSum = i;
-        }
-    }
-}
-Console.WriteLine($"Строка {rowWithMinSum}, сумма {minSum}");
+GetRowNumberAndSum(array);
+Console.WriteLine();
